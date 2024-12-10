b839655a3fb27ebdd4976b75ec18e1d45381de1e
Aleshua <snleha19@gmail.com>
2024-12-10 16:09:56 +0600

    [+] 7th IS 5lab

---
+++ 7th semester/java/lab7/src/Main.java
@@ -0,0 +1,11 @@
+package src;
+
+import java.util.concurrent.ExecutionException;
+
+public class Main {
+
+    public static void main(String[] args) throws InterruptedException, ExecutionException {
+
+
+    }
+}
---
+++ 7th semester/java/lab7/src/Mapper.java
@@ -0,0 +1,13 @@
+package src;
+
+import java.io.IOException;
+
+import org.w3c.dom.Text;
+
+public class Mapper {
+
+    public void map(Object key, Text value, Context context) throws IOException, InterruptedException {
+
+    }
+
+}
